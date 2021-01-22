9c362b1aee462042a66e07b9dc4676cfc977c2c5
Batuhan337tr <47800213+Batuhan337tr@users.noreply.github.com>
2021-01-22 17:55:52 +0300

    assets

---
+++ assets/inc/Settings.php
@@ -0,0 +1,10 @@
+<?php
+
+include_once 'Rcon.php';
+
+$host = 'localhost'; // RCON HOST
+$port = '25575'; //RCON PORT DEFAULT 25575
+$password = 'denemeasd'; //PASSWORD YOUR PASSWORD (in server.properties)
+$timeout = 3; // TIMEOUT
+
+?>
